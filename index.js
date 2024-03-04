695d47cb19849837355417d84894abc90bbe42de
VICTOR ROSADO <VGROSADO04@GMAIL.COM>
2024-03-04 16:17:11 -0500

    added dark theme background for body

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
-
+const body = document.querySelector('.body');
 const themeWrapper = document.querySelector('.header__theme-wrapper');
-const theme = document.querySelector('.header__theme')
+const theme = document.querySelector('.header__theme');
+const lightIcon = document.getElementById('#light');
+const darkIcon = document.getElementById('#dark');
 themeWrapper.addEventListener('click', handleTheme)
-let light = true;
+let light = false;
 
 //Set theme from light to dark
 function handleTheme() {
     if (!light) {
-        theme.innerText = 'Dark'
+        darkIcon.classList.remove('none');
+        lightIcon.classList.add('none');
         theme.classList.add('selected');
+        body.classList.add('dark');
         light = true;
     }
     else {
+        darkIcon.classList.add('none');
+        lightIcon.classList.remove('none');
         theme.classList.remove('selected');
-        theme.innerText = 'Light'
+        body.classList.remove('dark');
         light = false;
     };
 };
