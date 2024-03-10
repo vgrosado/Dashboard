fc6f69c27d062a2ccbb79a34ec7023613c49a376
VICTOR ROSADO <VGROSADO04@GMAIL.COM>
2024-03-09 19:39:02 -0500

    created asset folder and began to impliment check against city weather and time of day

--- index.js
+++ index.js
@@ -1,43 +1,44 @@
 const APIKEY = "bedd81aab35b46aee7f7356339be92b1";
 let city = "";
+let weather;
 let forecast;
-let hourlyForecast;
-let userForecast;
-let today = new Date();
-let weekday = new Intl.DateTimeFormat("en-us", { weekday: "long" }).format(today);
+let timeOfDay = new Date().toLocaleTimeString('en-US', {
+    hour: 'numeric', minute: 'numeric', hour12: true 
+});
+let convertTime = new Date();
+let weekday = new Intl.DateTimeFormat("en-us", { weekday: "long" }).format(new Date());
 let userLatitude = null;
 let userLongitude = null;
 const loading = document.querySelector('.forecast__spinner');
 const locationIcon = document.getElementById('#locationicon')
-const gif = document.getElementById('#gif');
+const weatherIcon = document.getElementById('#weathergif');
 
-    // get users current weather
-    function getUserWeather(lat, lon){
-        axios 
-            .get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIKEY}`)
-            .then((response) => {
-                forecast = response.data;
-                console.log(forecast)
-                updateWeather(forecast)
-                loading.classList.add('no')
-                gif.classList.remove('no')
-                locationIcon.classList.remove('no')
-            }).then(() => {
-                axios
+// get users current weather
+function getUserWeather(lat, lon) {
+    axios
+        .get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIKEY}`)
+        .then((response) => {
+            weather = response.data;
+            updateWeather(weather)
+            loading.classList.add('no')
+            weatherIcon.classList.remove('no')
+            locationIcon.classList.remove('no')
+        }).then(() => {
+            axios
                 .get(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKEY}`)
                 .then((response) => {
-                    hourlyForecast = response.data;
-                    console.log(hourlyForecast)
-                hourlyForecast = response.data;
-                loading.classList.add('no')
-                gif.classList.remove('no')
-                locationIcon.classList.remove('no')
-            })
-            }).catch((error) => {
-                console.error("Error fetching data:", error);
-            });
-    };
+                    forecast = response.data;
+                    console.log(weather)
+                    loading.classList.add('no')
+                    weatherIcon.classList.remove('no')
+                    locationIcon.classList.remove('no')
+                })
+        }).catch((error) => {
+            console.error("Error fetching data:", error);
+        });
+};
 
+    //access user device's GPS
     async function getPosition() { 
         navigator.geolocation.getCurrentPosition((position) => {
         userLatitude = position.coords.latitude;
@@ -45,13 +46,14 @@ const gif = document.getElementById('#gif');
         console.log(userLatitude + "" + userLongitude)
         getUserWeather(userLatitude, userLongitude);
         })
-        loading.src = "spinner.gif";
+        loading.src = "./Assets/spinner.gif";
         loading.classList.remove('no')
-        gif.classList.add('no')
+        weatherIcon.classList.add('no')
         locationIcon.classList.add('no')
     };
     getPosition();
 
+    //triggers api call based on search input
 const searchInput = document.getElementById('#search');
 searchInput.addEventListener('change', (event) => {
     city = event.target.value;
@@ -59,18 +61,30 @@ searchInput.addEventListener('change', (event) => {
     handleSearch(city)
 })
 
+    //function to take city input and call api for weather
 function handleSearch(city) {
-    if (!city) return; 
-        axios
-            .get(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIKEY}`)
-            .then((response) => {
-                forecast = response.data;
-                updateWeather(forecast)
-            })
-            .catch((error) => {
-                console.error("Error fetching data:", error);
-            });
-    };
+    if (!city) return;
+    axios
+        .get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}`)
+        .then((response) => {
+            weather = response.data;
+            console.log(weather)
+            updateWeather(weather)
+        }).then(() => {
+            axios
+                .get(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIKEY}`)
+                .then((response) => {
+                    forecast = response.data;
+                    console.log(weather)
+                    loading.classList.add('no')
+                    weatherIcon.classList.remove('no')
+                    locationIcon.classList.remove('no')
+                })
+        })
+        .catch((error) => {
+            console.error("Error fetching data:", error);
+        });
+};
 
 
 const body = document.querySelector('.body');
@@ -99,33 +113,38 @@ function handleTheme() {
     };
 };
 
-function updateWeather(forecast) {
-    
-    const location = document.getElementById('#location')
-    if (forecast.city) {
-    location.innerText = forecast.city.name;
-    } else if (forecast.name) {
-        location.innerText = forecast.name;
+// update weather card with weather variable
+function updateWeather(weather) {
+    let sunset = new Date(weather.sys.sunset * 1000).toLocaleTimeString();
+    let currentTime = convertTime.toLocaleTimeString();
+
+    const temp = document.getElementById('#temp')
+        temp.innerText = Math.floor(parseInt((weather.main.temp - 273.15) * 9/5 + 32)) + "°";
+
+    //set weather icon, checks against current city conditions & local time vs sunset
+    if (weather.weather[0].main.includes("Clouds") && sunset < currentTime) {
+    weatherIcon.src = "./Assets/partly-cloudy-night.svg"
+    } else if (weather.weather[0].main.includes("Clouds") && sunset > currentTime) {
+        weatherIcon.src = "./Assets/partly-cloudy-day.svg"
+    } else if (weather.weather[0].main.includes("Rain") && sunset < currentTime ) {
+        weatherIcon.src = "./Assets/partly-cloudy-night-rain.svg"
+    } else if (weather.weather[0].main.includes("Rain") && sunset> currentTime) {
+        weatherIcon.src = "./Assets/partly-cloudy-day-rain.svg"
     }
+
+    const details = document.getElementById('#details')
+        details.innerText = weather.weather[0].description;
+    
+
+    const location = document.getElementById('#location')
+        location.innerText = weather.name;
 
     const date = document.getElementById('#date')
     date.innerText = weekday;
 
     const time = document.getElementById('#time')
-    time.innerText = today.toLocaleTimeString(undefined, {
-        hour: 'numeric', minute: 'numeric', hour12: true 
-    });
-
-    const temp = document.getElementById('#temp')
-    if (forecast.list) {
-    temp.innerText =  Math.floor(parseInt((forecast.list[0].main.temp - 273.15) * 9/5 + 32)) + "°";
-    } else if (forecast.main.temp) {
-        temp.innerText = Math.floor(parseInt((forecast.main.temp - 273.15) * 9/5 + 32)) + "°";
-    }
-
-    const details = document.getElementById('#details')
-    details.innerText = forecast.weather[0].description;
-    }
+        time.innerText = timeOfDay;
+}
 
 
 
